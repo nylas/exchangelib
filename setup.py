e6e8ae18ba0c35faaf1baaf2a050a0c5623712be
Erik Cederstrand <erik@cederstrand.dk>
2016-05-12 23:42:51 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='exchangelib',
-    version='1.3.3',
+    version='1.3.4',
     author='Erik Cederstrand',
     author_email='erik@cederstrand.dk',
     description='Client for Microsoft Exchange Web Services (EWS)',
