4cb64c0209661d153993473c7d046559a53248fa
Erik Cederstrand <erik@cederstrand.dk>
2016-07-29 13:36:53 +0200

    Latest commits break enough public interfaces to require a point release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='exchangelib',
-    version='1.4.2',
+    version='1.5.0',
     author='Erik Cederstrand',
     author_email='erik@cederstrand.dk',
     description='Client for Microsoft Exchange Web Services (EWS)',
