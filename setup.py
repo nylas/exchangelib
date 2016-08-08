14bc073a20e27f0e6d82e6d7a02d69fd48023c31
Erik Cederstrand <erik@cederstrand.dk>
2016-08-08 18:06:09 +0200

    Update version again - fumbled the upload to PyPI

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='exchangelib',
-    version='1.5.1',
+    version='1.5.2',
     author='Erik Cederstrand',
     author_email='erik@cederstrand.dk',
     description='Client for Microsoft Exchange Web Services (EWS)',
