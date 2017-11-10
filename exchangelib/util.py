cd891106ed50eaaf72c6e5307802c95c95bbd048
Erik Cederstrand <erik@cederstrand.dk>
2017-11-10 09:56:45 +0100

    Add locking around _deepcopy_cache. Use same key for existence check and inserting. Refs #286

--- exchangelib/util.py
+++ exchangelib/util.py
@@ -5,6 +5,7 @@ from decimal import Decimal
 import io
 import itertools
 import logging
+from multiprocessing import Lock
 import re
 import socket
 import time
@@ -208,14 +209,19 @@ def safe_xml_value(value, replacement='?'):
 
 # Keeps a cache of Element objects to deepcopy
 _deepcopy_cache = dict()
+_deepcopy_cache_lock = Lock()
 
 
 def create_element(name, **attrs):
     # copy.deepcopy() is an order of magnitude faster than creating a new Element() every time
     key = (name, tuple(attrs.items()))  # dict requires key to be immutable
-    if name not in _deepcopy_cache:
-        _deepcopy_cache[key] = Element(name, **attrs)
-    return deepcopy(_deepcopy_cache[key])
+    try:
+        cached_elem = _deepcopy_cache[key]
+    except KeyError:
+        with _deepcopy_cache_lock:
+            # Use setdefault() because another thread may have filled the cache while we were waiting for the lock
+            cached_elem = _deepcopy_cache.setdefault(key, Element(name, **attrs))
+    return deepcopy(cached_elem)
 
 
 def add_xml_child(tree, name, value):
