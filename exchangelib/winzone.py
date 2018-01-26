9da6f29d24c3b0b8f7cfa515dadc0a42f98989a8
Erik Cederstrand <erik@cederstrand.dk>
2018-01-26 09:28:21 +0100

    New timezone for São Tomé

--- exchangelib/winzone.py
+++ exchangelib/winzone.py
@@ -69,7 +69,7 @@ CLDR_TO_MS_TIMEZONE_MAP = {
     'Africa/Nouakchott': 'Greenwich Standard Time',
     'Africa/Ouagadougou': 'Greenwich Standard Time',
     'Africa/Porto-Novo': 'W. Central Africa Standard Time',
-    'Africa/Sao_Tome': 'Greenwich Standard Time',
+    'Africa/Sao_Tome': 'W. Central Africa Standard Time',
     'Africa/Tripoli': 'Libya Standard Time',
     'Africa/Tunis': 'W. Central Africa Standard Time',
     'Africa/Windhoek': 'Namibia Standard Time',
