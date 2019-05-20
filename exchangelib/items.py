fc8d3393b0c3859f51b5116d72ddcb97363ed270
Erik Cederstrand <erik@cederstrand.dk>
2019-05-20 13:18:56 +0200

    Don't reusre the same FIELDS object

--- exchangelib/items.py
+++ exchangelib/items.py
@@ -1149,7 +1149,7 @@ class MeetingResponse(BaseMeetingItem):
     MSDN: https://msdn.microsoft.com/en-us/library/office/aa564337(v=exchg.150).aspx
     """
     ELEMENT_NAME = 'MeetingResponse'
-    FIELDS = Message.FIELDS
+    FIELDS = Message.FIELDS.copy()
 
 
 class MeetingCancellation(BaseMeetingItem):
