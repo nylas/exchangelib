609e21eb24d1a5a0c61599db26828a5122481bf8
Erik Cederstrand <erik@cederstrand.dk>
2019-10-29 15:04:11 +0100

    No need to copy, we're not modifying argv anymore

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,8 +8,7 @@ from exchangelib.util import PrettyXmlHandler
 # Always show full repr() output for object instances in unittest error messages
 unittest.util._MAX_LENGTH = 2000
 
-argv = sys.argv.copy()
-if '-v' in argv:
+if '-v' in sys.argv:
     logging.basicConfig(level=logging.DEBUG, handlers=[PrettyXmlHandler()])
 else:
     logging.basicConfig(level=logging.CRITICAL)
