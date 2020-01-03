b2f7006835393132bdf2cfb416ba44f847c8bbf2
Erik Cederstrand <erik@cederstrand.dk>
2020-01-03 15:17:33 +0100

    Attempt to fix wipe script in Travis

--- scripts/wipe_test_account.py
+++ scripts/wipe_test_account.py
@@ -1,5 +1,5 @@
-if __name__ == '__main__':
-    from tests.common import EWSTest
-    t = EWSTest()
-    t.setUpClass()
-    t.wipe_test_account()
+from tests.common import EWSTest
+
+t = EWSTest()
+t.setUpClass()
+t.wipe_test_account()
