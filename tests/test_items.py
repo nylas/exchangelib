ad7801734cd3cc0b0223206b98047962f2e99154
Erik Cederstrand <erik@cederstrand.dk>
2020-01-08 07:43:31 +0100

    Task validation test fits better in TasksTest

--- tests/test_items.py
+++ tests/test_items.py
@@ -34,38 +34,6 @@ from .common import EWSTest, TimedTestCase, get_random_string, get_random_dateti
     get_random_email, get_random_decimal, get_random_choice, mock_version
 
 
-class ItemTest(TimedTestCase):
-    def test_task_validation(self):
-        tz = EWSTimeZone.timezone('Europe/Copenhagen')
-        task = Task(due_date=tz.localize(EWSDateTime(2017, 1, 1)), start_date=tz.localize(EWSDateTime(2017, 2, 1)))
-        task.clean()
-        # We reset due date if it's before start date
-        self.assertEqual(task.due_date, tz.localize(EWSDateTime(2017, 2, 1)))
-        self.assertEqual(task.due_date, task.start_date)
-
-        task = Task(complete_date=tz.localize(EWSDateTime(2099, 1, 1)), status=Task.NOT_STARTED)
-        task.clean()
-        # We reset status if complete_date is set
-        self.assertEqual(task.status, Task.COMPLETED)
-        # We also reset complete date to now() if it's in the future
-        self.assertEqual(task.complete_date.date(), UTC_NOW().date())
-
-        task = Task(complete_date=tz.localize(EWSDateTime(2017, 1, 1)), start_date=tz.localize(EWSDateTime(2017, 2, 1)))
-        task.clean()
-        # We also reset complete date to start_date if it's before start_date
-        self.assertEqual(task.complete_date, task.start_date)
-
-        task = Task(percent_complete=Decimal('50.0'), status=Task.COMPLETED)
-        task.clean()
-        # We reset percent_complete to 100.0 if state is completed
-        self.assertEqual(task.percent_complete, Decimal(100))
-
-        task = Task(percent_complete=Decimal('50.0'), status=Task.NOT_STARTED)
-        task.clean()
-        # We reset percent_complete to 0.0 if state is not_started
-        self.assertEqual(task.percent_complete, Decimal(0))
-
-
 class BaseItemTest(EWSTest):
     TEST_FOLDER = None
     FOLDER_CLASS = None
@@ -2569,6 +2537,36 @@ class TasksTest(CommonItemTest):
     TEST_FOLDER = 'tasks'
     FOLDER_CLASS = Tasks
     ITEM_CLASS = Task
+
+    def test_task_validation(self):
+        tz = EWSTimeZone.timezone('Europe/Copenhagen')
+        task = Task(due_date=tz.localize(EWSDateTime(2017, 1, 1)), start_date=tz.localize(EWSDateTime(2017, 2, 1)))
+        task.clean()
+        # We reset due date if it's before start date
+        self.assertEqual(task.due_date, tz.localize(EWSDateTime(2017, 2, 1)))
+        self.assertEqual(task.due_date, task.start_date)
+
+        task = Task(complete_date=tz.localize(EWSDateTime(2099, 1, 1)), status=Task.NOT_STARTED)
+        task.clean()
+        # We reset status if complete_date is set
+        self.assertEqual(task.status, Task.COMPLETED)
+        # We also reset complete date to now() if it's in the future
+        self.assertEqual(task.complete_date.date(), UTC_NOW().date())
+
+        task = Task(complete_date=tz.localize(EWSDateTime(2017, 1, 1)), start_date=tz.localize(EWSDateTime(2017, 2, 1)))
+        task.clean()
+        # We also reset complete date to start_date if it's before start_date
+        self.assertEqual(task.complete_date, task.start_date)
+
+        task = Task(percent_complete=Decimal('50.0'), status=Task.COMPLETED)
+        task.clean()
+        # We reset percent_complete to 100.0 if state is completed
+        self.assertEqual(task.percent_complete, Decimal(100))
+
+        task = Task(percent_complete=Decimal('50.0'), status=Task.NOT_STARTED)
+        task.clean()
+        # We reset percent_complete to 0.0 if state is not_started
+        self.assertEqual(task.percent_complete, Decimal(0))
 
     def test_complete(self):
         item = self.get_test_item().save()
